2cbd4e6ea26e63a1b82281296fbb3618f1d2a807
Amri Toufali <amri@handsomemedia.com>
2023-08-25 16:11:36 -0700

    add "details" element for advanced filter

--- server/views/partials/moviesList.js
+++ server/views/partials/moviesList.js
@@ -27,7 +27,7 @@ const withoutGenres = data => {
 const sortOptions = [['Popularity', 'popularity.desc'], ['Most Recent', 'primary_release_date.desc'], ['Score', 'vote_average.desc']]
 const sortRadiolist = data => sortOptions.reduce((acc, cur) => {
   acc += `
-  <label>
+  <label class='pill'>
     <input type='radio' name='sort' value='${cur[1]}' ${data.sort.includes(cur[1]) ? 'checked' : ''}>
     <span>${cur[0]}</span>
   </label>
@@ -36,18 +36,11 @@ const sortRadiolist = data => sortOptions.reduce((acc, cur) => {
 }, ``)
 
 export const moviesList = data => `
+<ul class='movies-list'></ul>
 <form action='/api/v1/movies' hidden>
   <fieldset>
     <h4>Include genres:</h4>
     ${withGenres(data)}
-  </fieldset>
-  <fieldset>
-    <h4>Exclude genres:</h4>
-    ${withoutGenres(data)}
-  </fieldset>
-  <fieldset>
-    <h4>Sort by:</h4>
-    ${sortRadiolist(data)}
   </fieldset>
   <fieldset>
     <h4>Minimum review score:</h4>
@@ -55,19 +48,33 @@ export const moviesList = data => `
       <input type="range" name="score" min="0" max="100" value="${data.reviewScore}" step="1">
       <output>${data.reviewScore}</output>
     </label>
-    <h4>Minimum review count:</h4>
-    <label>
-      <input type="range" name="count" min="10" max="1000" value="${data.reviewCount}" step="10">
-      <output>${data.reviewCount}</output>
-    </label>
+  </fieldset>
+  <fieldset>
     <h4>Release date:</h4>
     <label>
       <range-slider name="years" min='1900' max='${data.currentYear}' value='${data.years}'></range-slider>
       <output>${data.years.split(',').join(' - ')}</output>
     </label>
   </fieldset>
-  <button class='primary'>APPLY</button>
+  <fieldset>
+    <h4>Sort by:</h4>
+    ${sortRadiolist(data)}
+  </fieldset>
+  <details>
+    <summary>Advanced</summary>
+    <fieldset>
+      <h4>Exclude genres:</h4>
+      ${withoutGenres(data)}
+    </fieldset>
+    <fieldset>
+      <h4>Minimum review count:</h4>
+      <label>
+        <input type="range" name="count" min="10" max="1000" value="${data.reviewCount}" step="10">
+        <output>${data.reviewCount}</output>
+      </label>
+    </fieldset>
+  </details>
+  <button class='primary' type='submit'>APPLY</button>
 </form>
-<ul class='movies-list'></ul>
 <button class='form-toggle primary' type='button'>FILTER</button>
 `
