d56b7c95e79f81315474b9a89388e0460367f34c
Amri Toufali <amri@handsomemedia.com>
2023-09-09 05:07:17 -0700

    houskeeping

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,6 +1,6 @@
 /* Use this file to load global scripts. Partial scripts should be loaded directly from the partial using <script type='module'> */
 
 import './components/rangeSlider.js'
-import './app.js'
+// import './app.js'
 import './matchMedia.js'
 import './resizeObserver.js'
