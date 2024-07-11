d860909b8fe3de150a059be3930b15c1ab4746ab
Amri Toufali <amri@handsomemedia.com>
2024-07-11 11:09:52 -0700

    refactor matchMedia attributes to classes

--- client/src/scripts/matchMedia.js
+++ client/src/scripts/matchMedia.js
@@ -1,8 +1,8 @@
 const mediaQueryMobile = window.matchMedia('(width <= 768px)') // this breakpoint is also set in vars.css
 
 function handleMediaQuery(e = mediaQueryMobile) {
-  document.documentElement.toggleAttribute('mobile', e.matches)
-  document.documentElement.toggleAttribute('desktop', !e.matches)
+  document.documentElement.classList.toggle('mobile', e.matches)
+  document.documentElement.classList.toggle('desktop', !e.matches)
 }
 
 mediaQueryMobile.addEventListener('change', handleMediaQuery)
