6b978f189bd8a31c6fb9b5672527146995004379
Amri Toufali <amri@handsomemedia.com>
2024-07-11 20:29:15 -0700

    add throttle to time util

--- client/src/scripts/utils/time.js
+++ client/src/scripts/utils/time.js
@@ -3,12 +3,38 @@
  * @param {function} func
  * @param {number} delay
  * @return {function}
- * @example `el.addEventListener('input', debounce(handleInput))` where `handleInput` is the function doing the work
+ * @example `el.addEventListener('input', debounce(handleInput))` where `handleInput` is the function to debounce
  */
 export function debounce(func, delay = 400) {
   let timeout
+
   return (...args) => {
     clearTimeout(timeout)
-    timeout = setTimeout(() => { func.apply(this, args); }, delay)
+    timeout = setTimeout(() => { func.apply(this, args) }, delay)
   };
 }
+
+/**
+ * Wraps a given function to throttle it by a delay in milliseconds.
+ * Function is called immediately as well as the final call made during throttle
+ * @param {function} func
+ * @param {number} delay
+ * @return {function}
+ * @example `el.addEventListener('scroll', throttle(handleScroll))` where `handleScroll` is the function to throttle
+ */
+export function throttle(func, delay = 300) {
+  let timeout, lastCall
+
+  return (...args) => {
+    if (!lastCall) func(...args) // first call
+
+    if (timeout) return lastCall = func
+
+    timeout = setTimeout(() => {
+      if (lastCall) lastCall(...args) // last call
+      lastCall = null
+      timeout = null
+    }, delay)
+  }
+}
+
