5ab6a059be998b7f72afd23b5d5131485090dc5c
Amri Toufali <amri@handsomemedia.com>
2024-07-11 20:28:36 -0700

    rename app.js to mainView.js

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,7 +1,7 @@
 /* Use this file to load global scripts. Partial scripts should be loaded directly from the partial using <script type='module'> */
 
 // import '../../../components/rangeSlider.js'
-import './app.js'
+import './mainView.js'
 import './matchMedia.js'
 import './resizeObserver.js'
 import './components/movieCard.js'
