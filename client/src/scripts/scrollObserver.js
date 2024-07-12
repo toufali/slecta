68730f414013daf9711c529529235d81ff16b285
Amri Toufali <amri@handsomemedia.com>
2024-07-12 14:50:27 -0700

    require differential before toggling header visibility

--- client/src/scripts/scrollObserver.js
+++ client/src/scripts/scrollObserver.js
@@ -7,6 +7,8 @@ window.addEventListener('scroll', throttle(handleScroll))
 function handleScroll(e) {
   if (!prevScrollY) return prevScrollY = window.scrollY // avoid scroll behavior on page reload
 
+  if (Math.abs(window.scrollY - prevScrollY) < 10) return // only handle when difference is greater than 10 lines
+
   const dir = window.scrollY > prevScrollY ? 'down' : 'up'
 
   document.documentElement.setAttribute('data-scroll-dir', dir)
